13461a33c49b78731164299ceb72a231fb4e0c18
patxi <patxiarana05@gmail.com>
2023-04-19 10:13:12 -0300

    styles in vars

--- IntroduccionJS/js/01.js
+++ IntroduccionJS/js/01.js
@@ -7,3 +7,21 @@ var disponible; // Iniciamos la variable sin valor
 var producto = true ; //Reasignando el valor de la variable
 
 disponible = true ;
+
+var producto1 = "computadora",
+disponible = true,
+categoria = "computadoras";
+
+//Var 1disponible;
+var disponible1_;
+
+
+// estilos para las varialbes
+
+var nombre_producto = "Monitor HD";//underscore
+var nombreProducto = "Monitor HD"; // camelcase
+var NombreProducto = "Monitor HD"; // Pascal Case
+var nombrepructo = "Monitor HD"; // 
+
+
+console.log(producto)
