e2d193d97f759f50b63e35c0931e9b5503302b2f
patxi <patxiarana05@gmail.com>
2023-04-19 11:39:22 -0300

    string

--- IntroduccionJS/js/02.js
+++ IntroduccionJS/js/02.js
@@ -1,29 +1,27 @@
-// Variables con let
-
 // Variables
 
-let producto = "Audifonos Gamers" // Iniciar Variable asignarle valor 
+var producto = "Audifonos Gamers" // Iniciar Variable asignarle valor 
 
-let disponible; // Iniciamos la variable sin valor
+var disponible; // Iniciamos la variable sin valor
 
-let producto2 = true ; //Reasignando el valor de la variable
+var producto = true ; //Reasignando el valor de la variable
 
 disponible = true ;
 
-let producto1 = "computadora",
-disponible1 = true,
+var producto1 = "computadora",
+disponible = true,
 categoria = "computadoras";
 
 //Var 1disponible;
-let disponible1_;
+var disponible1_;
 
 
 // estilos para las varialbes
 
-let nombre_producto = "Monitor HD";//underscore
-let nombreProducto = "Monitor HD"; // camelcase
-let NombreProducto = "Monitor HD"; // Pascal Case
-let nombrepructo = "Monitor HD"; // 
+var nombre_producto = "Monitor HD";//underscore
+var nombreProducto = "Monitor HD"; // camelcase
+var NombreProducto = "Monitor HD"; // Pascal Case
+var nombrepructo = "Monitor HD"; // 
 
 
-console.log(producto)   
+console.log(producto)
