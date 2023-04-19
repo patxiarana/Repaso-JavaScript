eab6daa1b4753295061d81c854aadcd70325c9bf
patxi <patxiarana05@gmail.com>
2023-04-19 09:54:02 -0300

    dinamic example

---
+++ IntroduccionJS/js/01.js
@@ -0,0 +1,9 @@
+// Variables
+
+var producto = "Audifonos Gamers" // Iniciar Variable asignarle valor 
+
+var disponible; // Iniciamos la variable sin valor
+
+var producto = true ; //Reasignando el valor de la variable
+
+disponible = true ;
