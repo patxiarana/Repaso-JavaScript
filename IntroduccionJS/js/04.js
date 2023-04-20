2bd95bd7ea185f9945c90e1e407bf5b70a9c7257
patxi <patxiarana05@gmail.com>
2023-04-20 08:52:06 -0300

    repaso

--- IntroduccionJS/js/04.js
+++ IntroduccionJS/js/04.js
@@ -1,13 +1,7 @@
 
-const producto = "monitor de 20 pulgadas";
 
-const producto2 = String('Monitor 30 Pulgadas')
 
-const producto3 = new String('Monitor 50 Pulgadas')
-
-const product4 = 'Monitor 20 pulgadas';
 
 console.log(producto)
-console.log(producto2)
-console.log(producto3)
+
 
---
+++ IntroduccionJS/js/05.js
@@ -0,0 +1,10 @@
+const numero1 = 100 ;
+const numero2 = 200;
+const numero3 = 300;
+const numero4 = 400 ;
+const numero5 = .1020;
+const numero6 = 5 ;
+
+
+console.log(numero1 + numero2) ;
+console.log(numero2 - numero1) ;
