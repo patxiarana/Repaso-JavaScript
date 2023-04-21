fcecdce439c18fb89458a1ae3577dc2dc34b9aeb
patxi <patxiarana05@gmail.com>
2023-04-21 09:34:48 -0300

    repaso

--- IntroduccionJS/js/06.js
+++ IntroduccionJS/js/06.js
@@ -5,7 +5,10 @@ resultado = Math.PI;
 resultado = Math.round(2.5)  ;
 resultado = Math.ceil(2.2) //Siempre redondea hacia arriba ;
 resultado = Math.floor // Flor siempre redondea hacia abajo ;
-
-
+resultado = Math.sqrt(144) // Raiz cuadrada ;
+resultado = Math.abs(-144) // Convierte un numero negativo a positivo ;
+resultado = Math.min(3,5,1,8,2,10); // Busca el minimo ;
+resultado = Math.max(3,5,1,8,2,10); // Busca el maximo ;
+resutlado = Math.random() ; // Genera un numero aleatorio ;
 
 console.log(resultado)
