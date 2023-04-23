51571a64c6c5812b02f52d0f6dde304e3a9ba8bd
patxi <patxiarana05@gmail.com>
2023-04-23 08:16:45 -0300

    repaso

--- IntroduccionJS/js/09.js
+++ IntroduccionJS/js/09.js
@@ -1,10 +1,12 @@
 // Objetos
-
+/*
 const nombreProducto = "Monitor 20 Pulgadas" ;
 const precio = 300 ;
 const disponible = true ;
-
+*/
 
 const producto = {
-    nombreProduto : "Monitor 20 Pulgadas"
+    nombreProduto : "Monitor 20 Pulgadas" ,
+    precio : 300 ,
+    disponible : true ,
 }
