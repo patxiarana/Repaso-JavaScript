bf00858203075b45f779b9341158f0f6d8ba9f1f
patxi <patxiarana05@gmail.com>
2023-04-23 08:06:56 -0300

    repaso

--- IntroduccionJS/js/08.js
+++ IntroduccionJS/js/08.js
@@ -1,9 +1,10 @@
 const boolean1 = true ;
 const boolean2 = false ;
-const boolean3 = "true" ;
+
 
 
 
 console.log(boolean1)
-console.log(boolean1)
-console.log(boolean1)
+console.log(boolean2)
+
+const boolean3 = new boolean(true)
