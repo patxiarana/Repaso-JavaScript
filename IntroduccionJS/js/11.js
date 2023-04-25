9a1740deaa648de424786472589d38f0a77db27a
patxi <patxiarana05@gmail.com>
2023-04-25 08:46:35 -0300

    repaso

---
+++ IntroduccionJS/js/11.js
@@ -0,0 +1,11 @@
+//This 
+
+const reservacion = {
+nombre : 'juan',
+apellido : 'De la Torre',
+total : 5000 ,
+pagado : false ,
+informacion: function(){
+console.log(`cliente${reservacion.nombre} total a pagar${reservacion.total}`)
+}
+}
