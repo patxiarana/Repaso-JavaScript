2affd7004c380ad1c8fa0ec1a2cd4debbefd9532
patxi <patxiarana05@gmail.com>
2023-04-25 09:30:21 -0300

    repaso

--- IntroduccionJS/js/12.js
+++ IntroduccionJS/js/12.js
@@ -16,6 +16,12 @@ function Producto(nombre, precio) {
 this.nombre = nombre,
 this.nombre = precio
 }
+
+Producto.prototype.fomatearProducto = function () {
+    return `El Poducto ${this.nombre} tiene un precion de: ${this.precio}`
+}
+
+
 // argumentos 
 const producto2 = new Producto('Monitor curvo',8000) ;
 const producto3 = new Producto('Laptot',8000) ;
@@ -23,3 +29,5 @@ const producto3 = new Producto('Laptot',8000) ;
 
 console.log(producto2)
 console.log(producto3)
+console.log(producto2.fomatearProducto())
+console.log(producto3.fomatearProducto())
