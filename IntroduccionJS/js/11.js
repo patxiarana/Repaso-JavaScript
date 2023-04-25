e143a0e93b36de6c3cf811a6a745adc246a15c13
patxi <patxiarana05@gmail.com>
2023-04-25 08:52:21 -0300

    repaso

--- IntroduccionJS/js/11.js
+++ IntroduccionJS/js/11.js
@@ -6,6 +6,20 @@ apellido : 'De la Torre',
 total : 5000 ,
 pagado : false ,
 informacion: function(){
-console.log(`cliente${reservacion.nombre} total a pagar${reservacion.total}`)
+console.log(`cliente${this.nombre} total a pagar${this.total}`)
 }
 }
+
+const reservacion2 = {
+    nombre : 'juan',
+    apellido : 'De la Torre',
+    total : 5000 ,
+    pagado : false ,
+    informacion: function(){
+    console.log(`cliente${this.nombre} total a pagar${this.total}`)
+    }
+    }
+
+
+    reservacion.informacion() ; 
+    reservacion2.informacion() ;
