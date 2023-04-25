749a5fcb7583af01086ede599c9ce00022389825
patxi <patxiarana05@gmail.com>
2023-04-25 09:16:06 -0300

    repaso

---
+++ IntroduccionJS/js/12.js
@@ -0,0 +1,25 @@
+//POO 
+
+
+/* Object literal */
+
+
+const product = {
+nombre : 'tablet',
+precio : 500 ,
+}
+
+// Objet Constructor 
+
+// parametros
+function Producto(nombre, precio) {
+this.nombre = nombre,
+this.nombre = precio
+}
+// argumentos 
+const producto2 = new Producto('Monitor curvo',8000) ;
+const producto3 = new Producto('Laptot',8000) ;
+
+
+console.log(producto2)
+console.log(producto3)
