6683380eec35aa71c9e9e3b220e6a78c2802b9cf
patxi <patxiarana05@gmail.com>
2023-04-26 09:26:55 -0300

    repaso

--- IntroduccionJS/js/14.js
+++ IntroduccionJS/js/14.js
@@ -8,5 +8,15 @@ if(auth){
 }
 })
 
+usuarioAutenticado
+.then(function(resultado) {
+    console.log(resultado)
+})
+
 
 console.log(usuarioAutenticado)
+
+// 3 Estados del promise
+// Penddingl: no se a cumplido ni a sido rechazado
+//fulffiled: ya se cumplio 
+// Reject se a rechazado o no se pudo cumplir 
